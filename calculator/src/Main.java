e3515eebfc82403f64f67f9cb14add4be02332b0
Icloves <ilyaoa19@gmail.com>
2022-10-12 19:15:58 +0300

    add frame

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,16 +1,18 @@
+package Calculator.calculator.src;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        int num1 = 0;
-        int num2 = 0;
+        int num1;
+        int num2;
 
         char operator;
         double answer = 0;
 
         Scanner input = new Scanner(System.in);
-
         System.out.println("enter first number");
         num1 = input.nextInt();
         System.out.println("enter second number");
@@ -19,25 +21,41 @@ public class Main {
         operator = input.next().charAt(0);
 
         switch (operator) {
-
             case '+':
                 answer = num1 + num2;
                 break;
-
             case '-':
                 answer = num1 - num2;
                 break;
-
             case '*':
                 answer = num1 * num2;
                 break;
-
             case '/':
                 answer = num1 / num2;
                 break;
-
         }
-        System.out.println(num1 + operator + num2 + answer);
+        System.out.println(answer);
+        calwin();
+    }
 
+    private static void calwin() {
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        JButton button = new JButton("Press");
+        JButton button1 = new JButton("press2");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.getContentPane().add(button1);
+        button1.addActionListener(new ListenerAction());
+        frame.setVisible(true);
+    }
+
+    static class ListenerAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            System.out.println("Нажатие кнопки от - " + e.getActionCommand());
+            System.out.println("Answer = " + e.getActionCommand());
+        }
     }
 }
