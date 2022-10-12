6e10e6c1377d1804dfd2aaf607493eacff21a71c
arman1-ba <ara.crock@mail.ru>
2022-10-12 17:47:20 +0300

    calculator java

---
+++ calculator/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
+
+        char operator;
+        double answer = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter first number");
+        num1 = input.nextInt();
+        System.out.println("enter second number");
+        num2 = input.nextInt();
+        System.out.println("What operation?");
+        operator = input.next().charAt(0);
+
+        switch (operator) {
+
+            case '+':
+                answer = num1 + num2;
+                break;
+
+            case '-':
+                answer = num1 - num2;
+                break;
+
+            case '*':
+                answer = num1 * num2;
+                break;
+
+            case '/':
+                answer = num1 / num2;
+                break;
+
+        }
+        System.out.println(num1 + operator + num2 + answer);
+
+    }
+}
