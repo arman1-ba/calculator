1411c4a6b7c64ce0ce60f3d40f9b34317c6b1432
arman1-ba <ara.crock@mail.ru>
2022-10-14 14:00:23 +0300

    add buttons

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -43,37 +43,89 @@ public class Main {
     private static void calwin() {
         JFrame frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        frame.setSize(370, 400);
 
-        JButton button = new JButton("Press");
-          button.setSize(80, 30);
-          button.setLocation(5,5);
+        JButton button = new JButton("+");
+          button.setSize(65, 60);
+          button.setLocation(5,50);
 
-        JButton button1 = new JButton("press2");
-          button1.setSize(80, 30);
-          button1.setLocation(60,60);
+        JButton button1 = new JButton("-");
+          button1.setSize(65, 60);
+          button1.setLocation(75,50);
 
         JButton button2 = new JButton("X");
-          button2.setSize(80, 30);
-          button2.setLocation(80,80);
+          button2.setSize(65, 60);
+          button2.setLocation(145,50);
 
         JButton button3 = new JButton("/");
-          button3.setSize(80, 30);
-          button3.setLocation(40,40);
+          button3.setSize(65, 60);
+          button3.setLocation(215,50);
+
+        JButton button4 = new JButton("DEL");
+          button4.setSize(65, 60);
+          button4.setLocation(285,50);
+
+        JButton button5 = new JButton("%");
+          button5.setSize(65, 60);
+          button5.setLocation(5,115);
+
+        JButton button6 = new JButton(".");
+          button6.setSize(65, 60);
+          button6.setLocation(75,115);
+
+        JButton button7 = new JButton("1");
+          button7.setSize(65, 60);
+          button7.setLocation(145,115);
+
+        JButton button8 = new JButton("2");
+          button8.setSize(65, 60);
+          button8.setLocation(215,115);
+
+        JButton button9 = new JButton("3");
+          button9.setSize(65, 60);
+          button9.setLocation(285,115);
+
+        JButton button10 = new JButton("(");
+          button10.setSize(65, 60);
+          button10.setLocation(5,180);
+
+        JButton button11 = new JButton(")");
+          button11.setSize(65, 60);
+          button11.setLocation(75,180);
+
+        JButton button12 = new JButton("sin");
+          button12.setSize(65, 60);
+          button12.setLocation(145,180);
 
         frame.getContentPane().add(button);
         frame.getContentPane().add(button1);
         frame.getContentPane().add(button2);
         frame.getContentPane().add(button3);
+        frame.getContentPane().add(button4);
+        frame.getContentPane().add(button5);
+        frame.getContentPane().add(button6);
+        frame.getContentPane().add(button7);
+        frame.getContentPane().add(button8);
+        frame.getContentPane().add(button9);
+        frame.getContentPane().add(button10);
+        frame.getContentPane().add(button11);
+        frame.getContentPane().add(button12);
 
         button.addActionListener(new ListenerAction());
         button1.addActionListener(new ListenerAction());
         button2.addActionListener(new ListenerAction());
         button3.addActionListener(new ListenerAction());
+        button4.addActionListener(new ListenerAction());
+        button5.addActionListener(new ListenerAction());
+        button6.addActionListener(new ListenerAction());
+        button7.addActionListener(new ListenerAction());
+        button8.addActionListener(new ListenerAction());
+        button9.addActionListener(new ListenerAction());
+        button10.addActionListener(new ListenerAction());
+        button11.addActionListener(new ListenerAction());
+        button12.addActionListener(new ListenerAction());
 
         frame.setVisible(true);
-
-
 
     }
 
